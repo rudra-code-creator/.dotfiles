c6d50ccd1416e39058f58887dde01dfa3ca0379a
Rudra Keshwani <rudrakeshwani2@gmail.com>
2024-12-29 04:16:28 +1000

    added over 500 wallpapers, repo size probably doubled due to this

--- user/app/terminal/kitty.nix
+++ user/app/terminal/kitty.nix
@@ -6,9 +6,24 @@
   home.packages = with pkgs; [
     kitty
   ];
-  programs.kitty.enable = true;
-  programs.kitty.settings = {
-    background_opacity = lib.mkForce "0.85";
-    modify_font = "cell_width 90%";
+  programs.kitty = {
+    enable = true;
+    package = pkgs.kitty;
+    settings = {
+      scrollback_lines = 2000;
+      wheel_scroll_min_lines = 1;
+      window_padding_width = 4;
+      confirm_os_window_close = 0;
+      background_opacity = lib.mkForce "0.85";
+      modify_font = "cell_width 90%";
+    };
+    extraConfig = ''
+      tab_bar_style fade
+      tab_fade 1
+      active_tab_font_style   bold
+      inactive_tab_font_style bold
+    '';
   };
 }
+
+
