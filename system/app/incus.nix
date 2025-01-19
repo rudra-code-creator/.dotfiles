b386841ccd3d3f81b1ba6e39deb93dd3c4113db3
Rudra Keshwani <>
2025-01-19 20:52:57 +1000

    finished configuring incus

--- system/app/incus.nix
+++ system/app/incus.nix
@@ -1,10 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     incus
   ];
 
   virtualisation.incus = {
     enable = true;
+    package = pkgs.incus-lts;
+    lxcPackage = config.virtualisation.lxc.package;
+    clientPackage = config.virtualisation.incus.package.client;
+    agent.enable = true;
+    softDaemonRestart = true;
 
     ui = {
       enable = true;
